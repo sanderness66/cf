be3e2481f9bd2a675be348ab599f117057358605
Sander van Malssen <svm@kozmix.org>
2022-02-18 17:12:50 +0100

    *** empty log message ***

--- cf.go
+++ cf.go
@@ -1,10 +1,15 @@
-// CF -- count files in directory and subdirectories (like du(1) but
+// CF -- count files in directories and subdirectories (like du(1) but
 // for counting files rather than file sizes)
 //
-// SvM 19-JAN-2021
+// SvM 30-JAN-2021 - 03-FEB-2021
 //
 // created from https://raw.githubusercontent.com/missedone/dugo/master/du.go
-// by chipping away everything we don't need
+// and https://golang.org/pkg/os/#Stat by chipping away anrything we
+// don't need
+
+// So, do we want to count a) only files, b) everything that isn't a
+// directory, or c) everything including directories? We seem to have
+// ended up doing b), but a) or c) is probably more useful.
 
 package main
 
@@ -14,45 +19,54 @@ import (
 	"os"
 )
 
-func diskUsage(currPath string, depth int) int64 {
-	var size int64
+var only_sum bool // whether -s option is active
+
+func count_files(currPath string, depth int) int64 {
+	var count int64
 
 	dir, err := os.Open(currPath)
 	if err != nil {
 		fmt.Println(err)
-		return size
+		return 0 // although there's a case for return 1, since it probably does exist
 	}
 	defer dir.Close()
 
-	files, err := dir.Readdir(-1)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	for _, file := range files {
-		if file.IsDir() {
-			size += diskUsage(fmt.Sprintf("%s/%s", currPath, file.Name()), depth+1)
+	stat, err := os.Stat(currPath)
+	switch mode := stat.Mode(); {
+	case mode.IsDir():
+		files, err := dir.Readdir(-1)
+		if err != nil {
+			fmt.Println(err)
 		} else {
-			size++
+			for _, file := range files {
+				if file.IsDir() {
+					count += count_files(fmt.Sprintf("%s/%s", currPath, file.Name()), depth+1)
+				} else {
+					count++
+				}
+			}
 		}
+	default:
+		count = 1
 	}
 
-	fmt.Printf("%d\t%s\n", size, currPath)
-	return size
+	if only_sum == false || (only_sum == true && depth == 0) {
+		fmt.Printf("%d\t%s\n", count, currPath)
+	}
+	return count
 }
 
 func main() {
 	var dir string
 
+	flag.BoolVar(&only_sum, "s", false, "display only a total for each argument")
 	flag.Parse()
 	if flag.NArg() > 0 {
 		for _, dir = range flag.Args() {
-			diskUsage(dir, 0)
+			count_files(dir, 0)
 		}
 	} else {
 		dir = "."
-		diskUsage(dir, 0)
+		count_files(dir, 0)
 	}
-
 }
